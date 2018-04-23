40b12cabbcdb25703c18ca81535e438bda7a9c1f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebApplication1/ReportService1.svc.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Runtime.Serialization
+Imports System.ServiceModel
+Imports System.ServiceModel.Activation
+Imports DevExpress.Data.Utils.ServiceModel
+Imports DevExpress.XtraReports.Service
+Imports DevExpress.XtraReports.UI
+
+Namespace WebApplication1
+    ' NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReportService1" in code, svc and config file together.
+    Public Class ReportService1
+        Inherits DevExpress.XtraReports.Service.ReportService
+
+    End Class
+End Namespace
